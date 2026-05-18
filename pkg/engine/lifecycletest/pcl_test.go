15500c40812ef3cf0e7bf636c93652ff5c4fe3e2
Fraser Waters <fraser@pulumi.com>
2026-05-18 09:39:24 +0100

    start bind test

--- pkg/engine/lifecycletest/pcl_test.go
+++ pkg/engine/lifecycletest/pcl_test.go
@@ -95,3 +95,73 @@ func TestPclSnippet(t *testing.T) {
 	require.Equal(t, tokens.Type("pulumi:providers:pkgA"), snap.Resources[0].Type)
 	require.Equal(t, tokens.Type("pkgA:index:res"), snap.Resources[1].Type)
 }
+
+// TestPclInvalidSnippet checks that an invalid snippet (i.e does not type check) returns an error.
+func TestPclInvalidSnippet(t *testing.T) {
+	t.Parallel()
+
+	loaders := []*deploytest.ProviderLoader{
+		deploytest.NewProviderLoader("pkgA", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+			return &deploytest.Provider{
+				GetSchemaF: func(ctx context.Context, gsr plugin.GetSchemaRequest) (plugin.GetSchemaResponse, error) {
+					return plugin.GetSchemaResponse{Schema: []byte(`{
+  "version": "0.0.1",
+  "name": "pkgA",
+  "resources": {
+    "pkgA:index:res": {
+      "inputProperties": {
+        "propA": { "type": "boolean" },
+        "propB": { "type": "string" }
+      },
+      "requiredInputs": ["propA", "propB"]
+    }
+  }
+}`)}, nil
+				},
+				CreateF: func(ctx context.Context, cr plugin.CreateRequest) (plugin.CreateResponse, error) {
+					uuid, err := uuid.NewV4()
+					if err != nil {
+						return plugin.CreateResponse{}, err
+					}
+					id := uuid.String()
+					if cr.Preview {
+						id = ""
+					}
+					return plugin.CreateResponse{
+						ID:         resource.ID(id),
+						Properties: cr.Properties,
+					}, nil
+				},
+			}, nil
+		}),
+	}
+
+	snippets := []resource.Snippet{
+		// only set one property
+		{Name: "test-resource", Type: "pkgA:index:res", Code: `propA = true`},
+	}
+
+	programF := deploytest.NewLanguageRuntimeF(func(_ plugin.RunInfo, monitor *deploytest.ResourceMonitor) error {
+		return nil
+	})
+
+	hostF := deploytest.NewPluginHostF(nil, nil, programF, loaders...)
+	p := &lt.TestPlan{
+		Options: lt.TestUpdateOptions{
+			SkipDisplayTests: true,
+			T:                t,
+			HostF:            hostF,
+		},
+	}
+
+	// Make an empty snapshot.
+	snap, err := lt.TestOp(Update).RunStep(
+		p.GetProject(), p.GetTarget(t, nil), p.Options, false, p.BackendClient, nil, "0")
+	require.NoError(t, err)
+
+	// Add a snippet to the snapshot and rerun the update to execute it.
+	snap.Snippets = snippets
+	snap, err = lt.TestOp(Update).RunStep(
+		p.GetProject(), p.GetTarget(t, snap), p.Options, false, p.BackendClient, nil, "1")
+	require.ErrorContains(t, err, "stuff")
+}
